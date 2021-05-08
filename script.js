9a584e296c09a28b193682d0935b81fc77fb0204
gui1949 <guilhermesantiago12@gmail.com>
2021-05-08 13:40:03 -0300

    Somente é inserido uma vez

--- script.js
+++ script.js
@@ -13,29 +13,29 @@
 let click_array = [];
 
 risco_click = (elem) => {
+
   pintar_botao = () => {
-    console.log(click_array);
+    click_array.push(elem.textContent);
     document.getElementById(elem.id).style.animationName = "clicar_risco";
     document.getElementById(elem.id).getElementsByTagName("p")[0].style.color =
       "#ffffff";
   };
 
-  click_array.push(elem.textContent);
-
-  if (click_array.length >= 5) {    
-    if (click_array.length == 5) {
-      pintar_botao();
-    }
-
-    if (window.matchMedia("(orientation: portrait)").matches) {
-      let proximo = document.getElementById("proximo");
-      proximo.style.display = "flex";
-      proximo.style.animationName = "prox_aparecer";
-    }
-    else{
-        $( ".draggable" ).draggable( "option", "disabled", true );
-    }
-  } else {
-    pintar_botao();
+  if(click_array.includes(elem.textContent) == false ){
+    if (click_array.length >= 4) {
+        if (click_array.length == 4) {
+          pintar_botao();
+        }
+    
+        if (window.matchMedia("(orientation: portrait)").matches) {
+          let proximo = document.getElementById("proximo");
+          proximo.style.display = "flex";
+          proximo.style.animationName = "prox_aparecer";
+        } else {
+          $(".draggable").draggable("option", "disabled", true);
+        }
+      } else {
+        pintar_botao();
+      }
   }
 };
