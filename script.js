6d6abf82d852d91ecbd6e6a3aac8404b0b89447e
Guilherme <guilhermesantiago12@gmail.com>
2021-05-09 19:46:56 -0300

    Auto Scroll

--- script.js
+++ script.js
@@ -13,7 +13,6 @@
 let click_array = [];
 
 risco_click = (elem) => {
-
   pintar_botao = () => {
     click_array.push(elem.textContent);
     document.getElementById(elem.id).style.animationName = "clicar_risco";
@@ -21,21 +20,28 @@ risco_click = (elem) => {
       "#ffffff";
   };
 
-  if(click_array.includes(elem.textContent) == false ){
+  if (click_array.includes(elem.textContent) == false) {
     if (click_array.length >= 4) {
-        if (click_array.length == 4) {
-          pintar_botao();
-        }
-    
-        if (window.matchMedia("(orientation: portrait)").matches) {
-          let proximo = document.getElementById("proximo");
-          proximo.style.display = "flex";
-          proximo.style.animationName = "prox_aparecer";
-        } else {
-          $(".draggable").draggable("option", "disabled", true);
-        }
-      } else {
+      if (click_array.length == 4) {
         pintar_botao();
       }
+
+      if (window.matchMedia("(orientation: portrait)").matches) {
+        let proximo = document.getElementById("proximo");
+        proximo.style.display = "flex";
+        proximo.style.animationName = "prox_aparecer";
+      } else {
+        $(".draggable").draggable("option", "disabled", true);
+        setTimeout(
+          () =>
+            document
+              .getElementById("listar_1")
+              .scrollIntoView({ block: "start", behavior: "smooth" }),
+          300
+        );
+      }
+    } else {
+      pintar_botao();
+    }
   }
 };
