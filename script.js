5e60b42684eee05f0887cdcbc2cf3356a1a4de1e
gui1949 <guilhermesantiago12@gmail.com>
2021-05-10 22:43:47 -0300

    Piramide mobile

--- script.js
+++ script.js
@@ -11,16 +11,53 @@
 // }
 
 let click_array = [];
+let dataText = ["Da lista abaixo, escolha cinco riscos."];
 
-risco_click = (elem) => {
-  pintar_botao = () => {
-    click_array.push(elem.textContent);
-    document.getElementById(elem.id).style.animationName = "clicar_risco";
-    document.getElementById(elem.id).getElementsByTagName("p")[0].style.color =
-      "#ffffff";
+document.addEventListener("DOMContentLoaded", function (event) {
+  typewrite();
+});
+
+typewrite = () => {
+  const typeWriter = (text, i, fnCallback) => {
+    if (i < text.length) {
+      document.getElementById("subtitulo1").innerHTML =
+        text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+      setTimeout(function () {
+        typeWriter(text, i + 1, fnCallback);
+      }, 100);
+    } else if (typeof fnCallback == "function") {
+      setTimeout(fnCallback, 700);
+    }
   };
 
-  if (click_array.includes(elem.textContent) == false) {
+  const StartTextAnimation = (i) => {
+    try {
+      if (i < dataText[i].length) {
+        typeWriter(dataText[i], 0, function () {
+          StartTextAnimation(i + 1);
+        });
+      }
+    } catch {}
+  };
+  StartTextAnimation(0);
+};
+
+pintar_botao = (elem) => {
+  document.getElementById(elem).style.animationName = "clicar_risco";
+  document.getElementById(elem).getElementsByTagName("p")[0].style.color =
+    "#ffffff";
+};
+
+risco_click = (elem) => {
+  let conteudoBTN = elem.textContent;
+  conteudoBTN = conteudoBTN.replace(/\t/g, "");
+  conteudoBTN = conteudoBTN.replace(/\n/g, "");
+
+  if (click_array.includes(conteudoBTN) == false) {
+    click_array.push(conteudoBTN);
+    pintar_botao(elem.id);
+
     if (click_array.length >= 4) {
       if (click_array.length == 4) {
         pintar_botao();
@@ -31,7 +68,6 @@ risco_click = (elem) => {
         proximo.style.display = "flex";
         proximo.style.animationName = "prox_aparecer";
       } else {
-        $(".draggable").draggable("option", "disabled", true);
         setTimeout(
           () =>
             document
@@ -44,4 +80,41 @@ risco_click = (elem) => {
       pintar_botao();
     }
   }
+};
+
+etapa2_mobile = (elem) => {
+  console.log(click_array);
+  document.getElementById(elem.id).style.animationName = "etapa1_desaparecer";
+  document.getElementById("riscos").style.animationName = "etapa1_desaparecer";
+
+  setTimeout(() => {
+    document.getElementById(elem.id).remove();
+    document.getElementById("riscos").remove();
+    document.getElementById("piramide").style.display = "flex";
+    document.getElementById("piramide").style.animationName = "prox_aparecer";
+    dataText.push("Mova os cinco riscos para a pirâmide.");
+    dataText.shift();
+    typewrite();
+
+    let riscos_mobile_lista = document.createElement("div");
+    $(".draggable").draggable("enabled");
+    riscos_mobile_lista.id = "riscos_mobile_lista";
+    document
+      .getElementById("piramide")
+      .insertAdjacentElement("afterend", riscos_mobile_lista);
+
+    let i = 0;
+
+    while (i < click_array.length) {
+      let riscos_mobile = document.createElement("div");
+      riscos_mobile.className = "draggable ui-widget-content";
+      riscos_mobile.id = "risco_mobile_" + i;
+      riscos_mobile.innerHTML = "<p>" + click_array[i] + "</p>";
+      let divAtual = document.getElementById("riscos_mobile_lista");
+      divAtual.appendChild(riscos_mobile);
+      riscos_mobile.onclick = () => pintar_botao(riscos_mobile.id);
+      i++;
+    }
+    $(".draggable").draggable({ snap: ".ui-widget-header" });
+  }, 900);
 };
